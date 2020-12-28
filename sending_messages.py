ea4ff381f7e9dd3960e3ee0dd8fbbcf9523e85db
Andrey Starozhilov <andrey.starozhilov@mail.ru>
2020-12-28 20:18:10 +0500

    Add argparse

--- sending_messages.py
+++ sending_messages.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import json
+import argparse
 
 
 logging.basicConfig(
@@ -44,8 +45,13 @@ async def authorise(reader, writer, account_hash):
     return credentials
 
 
-async def register(reader, writer, nickname):
+async def register(host, port, nickname):
     """Register a new user in the chat and get a account_hash"""
+    reader, writer = await asyncio.open_connection(
+        host,
+        port,
+    )
+
     data = await reader.readline()
     logging.info(data)
 
@@ -70,14 +76,54 @@ async def register(reader, writer, nickname):
     return account_hash
 
 
-async def main():
+async def main(host, port, token, username, message):
     reader, writer = await asyncio.open_connection(
-        'minechat.dvmn.org',
-        5050,
+        host,
+        port,
     )
-    await authorise(reader, writer, '')
-    await submit_message(reader, writer, 'Привет всем!!!!')
+    if not token:
+        token = await register(host, port, username)
+    await authorise(reader, writer, token)
+    await submit_message(reader, writer, message)
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    parser = argparse.ArgumentParser(
+        description='Program for authorization and sending messages in chat'
+    )
+    parser.add_argument(
+        '--host',
+        help='Host to connect',
+        default='minechat.dvmn.org'
+    )
+    parser.add_argument(
+        '--port',
+        help='Port to connect',
+        type=int,
+        default=5050
+    )
+    parser.add_argument(
+        '--token',
+        help='Authorization token',
+        default=None
+    )
+    parser.add_argument(
+        '--username',
+        help='Username for register a new user',
+        default=''
+    )
+    parser.add_argument(
+        '--message',
+        help='Message for chat',
+        default='Hello World!'
+    )
+    args = parser.parse_args()
+    host = args.host
+    port = args.port
+    token = args.token
+    username = args.username
+    message = args.message
+
+    asyncio.run(
+        main(host, port, token, username, message)
+    )
