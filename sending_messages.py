a62ee496a32ff8c50a9c6c2635a7ccebadde8dc7
Andrey Starozhilov <andrey.starozhilov@mail.ru>
2020-12-28 17:37:08 +0500

    Add script for user registration

--- sending_messages.py
+++ sending_messages.py
@@ -24,7 +24,7 @@ async def login_to_chat(user_token):
         formatted_data = data.decode().strip()
 
         if formatted_data == 'null':
-            assert json.loads(f'{formatted_data}') is not None, \
+            assert json.loads(formatted_data) is not None, \
                 'Неизвестный токен. ' \
                 'Проверьте его или зарегистрируйте заново.'
 
@@ -33,5 +33,31 @@ async def login_to_chat(user_token):
         writer.write('Hello, I Andrew!!!!\n\n'.encode())
         await writer.drain()
 
+
+async def register(host, port):
+    """Register a new user in the chat and get a token"""
+    reader, writer = await asyncio.open_connection(
+        host,
+        port,
+    )
+    writer.write('\n\n'.encode())
+    await writer.drain()
+
+    while True:
+        data = await reader.readline()
+        try:
+            user_credentials = json.loads(data)
+            account_hash = user_credentials.get('account_hash')
+
+            writer.close()
+            await writer.wait_closed()
+
+            return account_hash
+
+        except json.decoder.JSONDecodeError:
+            logging.warning('Invalid format to json')
+
+
 if __name__ == '__main__':
-    asyncio.run(login_to_chat('my_token'))
+    # asyncio.run(login_to_chat(''))
+    asyncio.run(register('minechat.dvmn.org', 5050))
