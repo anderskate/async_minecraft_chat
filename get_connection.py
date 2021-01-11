38d6c8a9a47be40221801c36d6e90f76ecfe7dd6
anderskate <andrey.starozhilov@mail.ru>
2021-01-11 18:51:52 +0500

    add feature to check connection

--- get_connection.py
+++ get_connection.py
@@ -1,19 +1,46 @@
 from contextlib import asynccontextmanager
 import asyncio
 import socket
+import requests
+import logging
+
+
+logger = logging.getLogger(__file__)
+
+
+def chat_is_available(host, port):
+    """Check the chat connection for the given host and port."""
+    try:
+        response = requests.get(f'http://{host}:{port}')
+        return True
+    except requests.exceptions.ConnectionError:
+        return False
 
 
 @asynccontextmanager
-async def get_connection(host, port):
+async def get_connection(host, port, timeout=0):
     """Get reader and writer objects.
 
     At the end of the work, be sure to close the writer object connection.
     """
+
+    while not chat_is_available(host, port):
+        chat_is_available(host, port)
+
+        # After an unsuccessful chat connection, 
+        # add another 10 seconds of timeout.
+        timeout += 10
+
+        logger.warning(
+            f'No chat connection. Reconnect after {timeout} seconds.'
+            )
+        await asyncio.sleep(timeout)
+
     reader, writer = await asyncio.open_connection(
-        host,
-        port,
-    )
-    
+            host,
+            port,
+        )
+
     try:
         yield reader, writer
     finally:
